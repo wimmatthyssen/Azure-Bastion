2b3370f6ca6d27717f4a90452c267d7572b3e5bf
Wim Matthyssen <62010638+wimmatthyssen@users.noreply.github.com>
2023-12-01 11:27:55 +0100

    Update Switch-AzureBastion-Standard-SKU-to-Basic-SKU.ps1

--- Switch-AzureBastion-Standard-SKU-to-Basic-SKU.ps1
+++ Switch-AzureBastion-Standard-SKU-to-Basic-SKU.ps1
@@ -1,34 +1,34 @@
 <#
 .SYNOPSIS
 
-A script used to switch an Azure Bastion host with Standard SKU to the Basic SKU.
+A script used to switch an Azure Bastion host with the Standard SKU to the Basic SKU.
 
 .DESCRIPTION
 
-A script used to switch an Azure Bastion host with Standard SKU to the Basic SKU.
+A script used to switch an Azure Bastion host with the Standard SKU to the Basic SKU.
 The script will do all of the following:
 
 Remove the breaking change warning messages.
 Change the current context to the subscription holding the Azure Bastion host, if the subscription exists; otherwise, exit the script.
-Save the Bastion host if it exists in the subscription as a variable and check if it uses the Basic SKU; if so, exit the script, otherwise the script will continue.
-Check if the Bastion resource group has a resource lock; if so, exit the script.
+Save the Bastion host if it exists in the subscription as a variable and check if it uses the Basic SKU; if so, exit the script; otherwise, the script will continue.
+Check if the Bastion resource group has a resource lock; if so, remove the resource lock.
 Store the specified set of Azure Bastion host tags in a hash table.
-Delete Azure Bastion host with Standard SKU.
-Redeploy same Azure Bastion host with Basic SKU.
+Delete the Azure Bastion host with the Standard SKU.
+Redeploy the same Azure Bastion host with the Basic SKU.
 Lock the Azure Bastion resource group with a CanNotDelete lock.
 
-** Keep in mind running this script can take up to 19 minutes. **
-** If you have a resource lock on the resource group holding the bastion host, remove it temporarily while running the script. **
+** Keep in mind that running this script can take up to 19 minutes. **
+** To remove an Azure resource lock, you'll need Owner or User Access Administrator permissions. **
 
 .NOTES
 
 Filename:       Switch-AzureBastion-Standard-SKU-to-Basic-SKU.ps1
 Created:        04/10/2022
-Last modified:  05/03/2023
+Last modified:  30/11/2023
 Author:         Wim Matthyssen
-Version:        2.3
-PowerShell:     Azure Cloud Shell or Azure PowerShell
-Requires:       PowerShell Az (v8.1.0) and Az.Network (v4.18.0)
+Version:        3
+PowerShell:     Azure PowerShell and Azure Cloud Shell
+Requires:       PowerShell Az (v10.4.1)
 Action:         Change variables were needed to fit your needs. 
 Disclaimer:     This script is provided "as is" with no warranties.
 
@@ -63,11 +63,12 @@ param(
 
 $bastionSkuBasic = "Basic"
 $bastionSkuStandard = "Standard"
+$lockName = #<your Bastion resource group resource lock name here> The existing Bastion resource group resource lock name. Example: "DoNotDeleteLock"
 
-$global:currenttime= Set-PSBreakpoint -Variable currenttime -Mode Read -Action {$global:currenttime= Get-Date -UFormat "%A %m/%d/%Y %R"}
+# Time, colors, and formatting
+Set-PSBreakpoint -Variable currenttime -Mode Read -Action {$global:currenttime = Get-Date -Format "dddd MM/dd/yyyy HH:mm"} | Out-Null 
 $foregroundColor1 = "Green"
 $foregroundColor2 = "Yellow"
-$foregroundColor3 = "Red"
 $writeEmptyLine = "`n"
 $writeSeperatorSpaces = " - "
 
@@ -75,7 +76,9 @@ $writeSeperatorSpaces = " - "
 
 ## Remove the breaking change warning messages
 
-Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true" | Out-Null
+Set-Item -Path Env:\SuppressAzurePowerShellBreakingChangeWarnings -Value $true | Out-Null
+Update-AzConfig -DisplayBreakingChangeWarning $false | Out-Null
+$warningPreference = "SilentlyContinue"
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -106,7 +109,7 @@ if ($subscriptionNotPresent) {
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Save the Bastion host if it exists in the subscription as a variable and check if it uses the Basic SKU; if so, exit the script, otherwise the script will continue
+## Save the Bastion host if it exists in the subscription as a variable and check if it uses the Basic SKU; if so, exit the script; otherwise, the script will continue
 
 $bastion = Get-AzBastion | Where-Object Name -Match $bastionName
 
@@ -137,20 +140,18 @@ Write-Host ($writeEmptyLine + "# Bastion host variable created" + $writeSeperato
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Check if the Bastion resource group has a resource lock; if so, exit the script
+## Check if the Bastion resource group has a resource lock; if so, remove the resource lock
 
-$lock = Get-AzResourceLock -ResourceGroupName $bastion.ResourceGroupName
 $rgNameBastion = $bastion.ResourceGroupName
+$lock = Get-AzResourceLock -ResourceGroupName $rgNameBastion
 
+# Check if resource lock exists
 if ($null -ne $lock){
-    Write-Host ($writeEmptyLine + "# Bastion resource group has a resource lock; please remove it and rerun the script" + $writeSeperatorSpaces + $currentTime)`
-    -foregroundcolor $foregroundColor3 $writeEmptyLine
-    Start-Sleep -s 3
-    Write-Host -NoNewLine ("# Press any key to exit the script ..." + $writeEmptyLine)`
-    -foregroundcolor $foregroundColor1 $writeEmptyLine;
-    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null;
-    return
-    } 
+    Write-Host ($writeEmptyLine + "# Bastion resource group $rgNameBastion has a resource lock; this will now be removed" + $writeSeperatorSpaces + $currentTime)`
+    -foregroundcolor $foregroundColor1 $writeEmptyLine
+    # Remove the resource lock
+    Remove-AzResourceLock -LockName $lockName -ResourceGroupName $bastion.ResourceGroupName -Force | Out-Null
+} 
 
 Write-Host ($writeEmptyLine + "# Resource group $rgNameBastion has no resource lock; the script will continue" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
@@ -159,14 +160,14 @@ Write-Host ($writeEmptyLine + "# Resource group $rgNameBastion has no resource l
 
 ## Store the specified set of Azure Bastion host tags in a hash table
 
-$bastionTags = (Get-AzResource -ResourceGroupName $bastion.ResourceGroupName -ResourceName $bastion.Name).Tags
+$bastionTags = (Get-AzResource -ResourceGroupName $rgNameBastion -ResourceName $bastion.Name).Tags
 
 Write-Host ($writeEmptyLine + "# Specified set of tags available to add" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine 
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Delete Bastion host with Standard SKU
+## Delete the Azure Bastion host with the Standard SKU
 
 Write-Host ($writeEmptyLine + "# Delete bastion host $bastionName with the $bastionSkuStandard SKU, which can take up to 8 minutes to complete" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor1 $writeEmptyLine
@@ -180,11 +181,25 @@ Write-Host ($writeEmptyLine + "# Bastion host $bastionName temporarily removed" 
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Redeploy Bastion host with Basic SKU
+## Redeploy the same Azure Bastion host with the Basic SKU
 
 $pipNameBastion = Get-AzPublicIpAddress -ResourceGroupName $bastion.ResourceGroupName
-$vnetName = Get-AzVirtualNetwork | ForEach-Object {if($_.Subnets.Name.Contains("AzureBastionSubnet")){return $_.Name}}
-$rgNameNetworking = (Get-AzVirtualNetwork | ForEach-Object {if($_.Subnets.Name.Contains("AzureBastionSubnet")){return $_.ResourceGroupName }}).Name
+
+# Get the virtual network with the AzureBastionSubnet
+$virtualNetwork = Get-AzVirtualNetwork | Where-Object {$_.Subnets -ne $null -and $_.Subnets.Name -contains "AzureBastionSubnet"}
+
+if ($null -ne $virtualNetwork) {
+    $vnetName = $virtualNetwork.Name
+    $rgNameNetworking = $virtualNetwork.ResourceGroupName
+} else {
+    Write-Host ($writeEmptyLine + "# Virtual network with 'AzureBastionSubnet' not found." + $writeSeperatorSpaces + $currentTime)`
+    -foregroundcolor $foregroundColor2 $writeEmptyLine
+    Start-Sleep -s 3
+    Write-Host -NoNewLine ("# Press any key to exit the script ..." + $writeEmptyLine)`
+    -foregroundcolor $foregroundColor1 $writeEmptyLine;
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null;
+    return
+}
 
 Write-Host ($writeEmptyLine + "# Redeploy bastion host $bastionName with $bastionSkuBasic SKU, which can take up to 10 minutes to complete" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor1 $writeEmptyLine
@@ -203,10 +218,10 @@ Write-Host ($writeEmptyLine + "# Bastion host $bastionName with $bastionSkuBasic
 
 ## Lock the Azure Bastion resource group with a CanNotDelete lock
 
-$lock = Get-AzResourceLock -ResourceGroupName $bastion.ResourceGroupName
+$lock = Get-AzResourceLock -ResourceGroupName $rgNameBastion
 
 if ($null -eq $lock){
-    New-AzResourceLock -LockName DoNotDeleteLock -LockLevel CanNotDelete -ResourceGroupName $bastion.ResourceGroupName -LockNotes "Prevent $rgNameBastion from deletion" -Force | Out-Null
+    New-AzResourceLock -LockName $lockName -LockLevel CanNotDelete -ResourceGroupName $rgNameBastion -LockNotes "Prevent $rgNameBastion from deletion" -Force | Out-Null
     } 
 
 Write-Host ($writeEmptyLine + "# Resource group $rgNameBastion locked" + $writeSeperatorSpaces + $currentTime)`
